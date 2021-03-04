22ad95192f23c82df0119c7ed1041594a290bdea
vengeang <therichdad101@gmail.com>
2021-03-04 10:54:57 +0700

    Add Bottom Navigation bar

---
+++ lib/screen/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          "Home Page",
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 // import 'package:myapp/mylogin_page.dart';
 // import 'package:myapp/student.dart';
 
@@ -8,59 +11,84 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _logo =
+      "https://p1.hiclipart.com/preview/743/354/454/avengers-age-of-ultron-logo-avengers-logo-png-clipart-thumbnail.jpg";
+  int _page = 0;
+  GlobalKey _bottomNavigationKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
-    // MyLogin login = MyLogin();
-    // login.myPassword = "d";
-    showMessege(Car("Toyota"));
-    showMessege(Bike("Giant"));
-    showMessege(Tuktuk("vespa"));
-    // print(Tuktuk("today"));
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dart OOP"),
-        centerTitle: true,
+      appBar: _buildAppBar,
+      body: _buildBody,
+      bottomNavigationBar: _buildButton,
+    );
+  }
+
+  get _buildAppBar {
+    return AppBar(
+      backgroundColor: Colors.red[400],
+      leading: Padding(
+        padding: const EdgeInsets.all(10),
+        child: InkWell(
+          onTap: () => print("Logo clicked"),
+          child: Image.network(_logo),
+        ),
       ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Text(
-          "login.hackPassword",
-          style: TextStyle(fontSize: 20),
+      title: Text("Interface Flutter"),
+      centerTitle: true,
+      actions: [
+        IconButton(
+          icon: Icon(Icons.person_search),
+          onPressed: () => print("search clicked"),
+        ),
+      ],
+    );
+  }
+
+  get _buildBody {
+    int numBer = _page + 1;
+    return Container(
+      color: Colors.blueAccent,
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Text(numBer.toString(), textScaleFactor: 10.0),
+            RaisedButton(
+              child: Text('Go To Page of index 1'),
+              onPressed: () {
+                final CurvedNavigationBarState navBarState =
+                    _bottomNavigationKey.currentState;
+                navBarState.setPage(0);
+              },
+            )
+          ],
         ),
       ),
     );
   }
 
-  showMessege(Vichicle v) {
-    v.display();
-  }
-}
-
-abstract class Vichicle {
-  String name;
-  Vichicle(this.name);
-  display();
-}
-
-class Car extends Vichicle {
-  Car(String name) : super(name);
-
-  display() {
-    return print(name);
-  }
-}
-
-class Bike extends Vichicle {
-  Bike(String name) : super(name);
-
-  @override
-  display() {}
-}
-
-class Tuktuk extends Vichicle {
-  Tuktuk(String name) : super(name);
-
-  @override
-  display() {}
-}
+  get _buildButton => CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        index: 0,
+        height: 50.0,
+        items: <Widget>[
+          Icon(Icons.add, size: 30),
+          Icon(Icons.list, size: 30),
+          Icon(Icons.compare_arrows, size: 30),
+          Icon(Icons.call_split, size: 30),
+          Icon(Icons.perm_identity, size: 30),
+        ],
+        color: Colors.white,
+        buttonBackgroundColor: Colors.white,
+        backgroundColor: Colors.blueAccent,
+        animationCurve: Curves.easeInOut,
+        animationDuration: Duration(milliseconds: 600),
+        onTap: (index) {
+          setState(() {
+            // Navigator.push(context, MaterialPageRoute(builder: context()=>HomeScreen(),));
+            _page = index;
+          });
+        },
+        letIndexChange: (index) => true,
+      );
+} //class home screen
